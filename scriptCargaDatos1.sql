e34bc3bab60ec92e77231bab8a1d7325c1eab32e
Juliana Tarditto <juliana-tarditto@hotmail.com>
2017-05-30 23:38:55 -0300

    Tablas Pers,Med,etc..

---
+++ scriptCargaDatos1.sql
@@ -0,0 +1,24 @@
+USE ChildrenCity;
+
+INSERT INTO Medicamento VALUES
+ ('Diclofenac'),
+ ('Ibuprofeno'),
+ ('Amoxicilina'),
+ ('Paracetamol'),
+ ('Dipirona');
+ 
+ INSERT INTO Visitante VALUES 
+ (34213567,'dni','Fenoglio','Marcelo'),
+ (34247175,'dni','Romero','Juana'),
+ (37643138,'dni','Ramirez','Carla'),
+ (35123571,'dni','Di Santo','Gonzalo'),
+ (32567124,'dni','Heredia','Maria');
+ 
+ INSERT INTO Personal VALUES
+ (23654124,'dni','Romero','Nicolas','Lamadrid 236',358440123),
+ (15234622,'dni','Gonzalez','Yamila','Colon 1921',358490235),
+ (25367124,'dni','Diaz','Camila','Mendoza 975',358497213),
+ (14257236,'dni','Lucero','Daniel','Buenos Aires 45',358480671),
+ (17286192,'dni','Martinez','German','Sobremonte 765',358476194);
+ 
+ 
