ad17c523f9c1eff672e9676301f9005cdab7ba90
Santiago <santimaria93@gmail.com>
2017-05-31 02:09:24 -0300

    script que borra todo el contenido de las tablas

---
+++ deleteAllRecords.sql
@@ -0,0 +1,60 @@
+SET SQL_SAFE_UPDATES = 0;
+
+delete from a_cargo;
+
+delete from Casa;
+
+delete from Episodio_Salud;
+
+delete from Historia_Clinica;
+
+delete from Legajo;
+
+delete from MAntecedente_Salud;
+
+delete from Medicamento;
+
+delete from Menor;
+
+delete from MTelPersonal;
+
+delete from Ocasion;
+
+delete from Personal;
+
+delete from Recetado;
+
+delete from Visita;
+
+delete from Visitante;
+
+ALTER TABLE a_cargo AUTO_INCREMENT = 1;
+
+ALTER TABLE Casa AUTO_INCREMENT = 1;
+
+ALTER TABLE Episodio_Salud AUTO_INCREMENT = 1;
+
+ALTER TABLE Historia_Clinica AUTO_INCREMENT = 1;
+
+ALTER TABLE Legajo AUTO_INCREMENT = 1;
+
+ALTER TABLE MAntecedente_Salud AUTO_INCREMENT = 1;
+
+ALTER TABLE Medicamento AUTO_INCREMENT = 1;
+
+ALTER TABLE Menor AUTO_INCREMENT = 1;
+
+ALTER TABLE MTelPersonal AUTO_INCREMENT = 1;
+
+ALTER TABLE Ocasion AUTO_INCREMENT = 1;
+
+ALTER TABLE Personal AUTO_INCREMENT = 1;
+
+ALTER TABLE Recetado AUTO_INCREMENT = 1;
+
+ALTER TABLE Visita AUTO_INCREMENT = 1;
+
+ALTER TABLE Visitante AUTO_INCREMENT = 1;
+
+SET SQL_SAFE_UPDATES = 1;
+    
