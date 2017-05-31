6cb6aa11d95e61686fa62204ec0d7def0cff938b
Juliana Tarditto <juliana-tarditto@hotmail.com>
2017-05-30 22:43:03 -0300

    modificando on delete

--- scriptCrearBaseD.sql
+++ scriptCrearBaseD.sql
@@ -32,8 +32,8 @@ CREATE TABLE `Menor` (
     `telefono` INTEGER,
     `nro_casa` INTEGER,
 	`nro_legajo` INTEGER,
-    CONSTRAINT `casaMenor` FOREIGN KEY (`nro_casa`) REFERENCES `Casa`(`nro_casa`),
-    CONSTRAINT `leg` FOREIGN KEY (`nro_legajo`) REFERENCES `Legajo`(`nro_legajo`)
+    CONSTRAINT `casaMenor` FOREIGN KEY (`nro_casa`) REFERENCES `Casa`(`nro_casa`) ON DELETE CASCADE,
+    CONSTRAINT `leg` FOREIGN KEY (`nro_legajo`) REFERENCES `Legajo`(`nro_legajo` ON DELETE CASCADE)
 )ENGINE InnoDB;
 
 
@@ -53,8 +53,8 @@ CREATE TABLE `MTelPersonal` (
     `nro_doc` INTEGER NOT NULL,
     `celular` INTEGER,
     PRIMARY KEY (`nro_doc`, `celular`),
-    CONSTRAINT `telPer` FOREIGN KEY (`nro_doc`) REFERENCES `Personal`(`nro_doc`)
-)ENGINE InnoDB; 
+    CONSTRAINT `telPer` FOREIGN KEY (`nro_doc`) REFERENCES `Personal`(`nro_doc`) ON DELETE CASCADE,
+)ENGINE InnoDB;
 
 
 DROP TABLE IF EXISTS `Visitante`;
@@ -72,8 +72,8 @@ CREATE TABLE `Visita` (
     `nro_visita` INTEGER,
     `dia_hora` DATETIME,
     PRIMARY KEY (`nro_doc_menor`, `nro_doc_visitante`),
-    CONSTRAINT `doc_menor` FOREIGN KEY (`nro_doc_menor`) REFERENCES `Menor`(`nro_doc`),
-    CONSTRAINT `doc_visitante` FOREIGN KEY (`nro_doc_visitante`) REFERENCES `Visitante`(`nro_doc`)
+    CONSTRAINT `doc_menor` FOREIGN KEY (`nro_doc_menor`) REFERENCES `Menor`(`nro_doc`) ON DELETE CASCADE,
+    CONSTRAINT `doc_visitante` FOREIGN KEY (`nro_doc_visitante`) REFERENCES `Visitante`(`nro_doc`) ON DELETE CASCADE
 )ENGINE InnoDB;
 
 
@@ -81,9 +81,9 @@ DROP TABLE IF EXISTS `a_cargo`;
 CREATE TABLE `a_cargo`(
 	`nro_casa` INTEGER NOT NULL,
     `nro_doc` INTEGER NOT NULL,
-    CONSTRAINT `doc` FOREIGN KEY (`nro_doc`) REFERENCES `Personal`(`nro_doc`),
-    CONSTRAINT `casa` FOREIGN KEY (`nro_casa`) REFERENCES `Casa`(`nro_casa`),
-    PRIMARY KEY (`nro_casa`, `nro_doc`)
+    CONSTRAINT `doc` FOREIGN KEY (`nro_doc`) REFERENCES `Personal`(`nro_doc`) ON DELETE CASCADE,
+    CONSTRAINT `casa` FOREIGN KEY (`nro_casa`) REFERENCES `Casa`(`nro_casa`) ON DELETE CASCADE,
+    PRIMARY KEY (`nro_casa`, `nro_doc`) ON DELETE CASCADE
 )ENGINE InnoDB;
 
 
@@ -93,8 +93,8 @@ CREATE TABLE `Ocasion`(
     `nro_doc` INTEGER NOT NULL,
     `fecha` DATE,
     PRIMARY KEY (`nro_casa`, `nro_doc`, `fecha`),
-	CONSTRAINT `doc_Ocasion` FOREIGN KEY (`nro_doc`) REFERENCES `Personal`(`nro_doc`),
-    CONSTRAINT `casa_Ocasion` FOREIGN KEY (`nro_casa`) REFERENCES `Casa`(`nro_casa`)
+	CONSTRAINT `doc_Ocasion` FOREIGN KEY (`nro_doc`) REFERENCES `Personal`(`nro_doc`) ON DELETE CASCADE,
+    CONSTRAINT `casa_Ocasion` FOREIGN KEY (`nro_casa`) REFERENCES `Casa`(`nro_casa`) ON DELETE CASCADE
 )ENGINE InnoDB;
 
 
@@ -102,7 +102,7 @@ DROP TABLE IF EXISTS `Historia_Clinica`;
 CREATE TABLE `Historia_Clinica`(
 	`nro_hist` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `nro_doc` INTEGER NOT NULL,
-    CONSTRAINT `dc` FOREIGN KEY (`nro_doc`) REFERENCES `Menor`(`nro_doc`)
+    CONSTRAINT `dc` FOREIGN KEY (`nro_doc`) REFERENCES `Menor`(`nro_doc`) ON DELETE CASCADE
 )ENGINE InnoDB;
 
 
@@ -110,7 +110,7 @@ DROP TABLE IF EXISTS `MAntecedente_Salud`;
 CREATE TABLE `MAntecedente_Salud`(
 	`nro_hist` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `antecedentes` VARCHAR(30),
-    CONSTRAINT `ant` FOREIGN KEY (`nro_hist`) REFERENCES `Historia_Clinica`(`nro_hist`)
+    CONSTRAINT `ant` FOREIGN KEY (`nro_hist`) REFERENCES `Historia_Clinica`(`nro_hist`) ON DELETE CASCADE
 )ENGINE InnoDB;
 
 
@@ -118,9 +118,9 @@ DROP TABLE IF EXISTS `Episodio_Salud`;
 CREATE TABLE `Episodio_Salud`(
 	`nro_item` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `nro_hist` INTEGER NOT NULL,
-    `descripcion` VARCHAR(50), 
+    `descripcion` VARCHAR(50),
     `fecha` DATE,
-     CONSTRAINT `histc` FOREIGN KEY (`nro_hist`) REFERENCES `Historia_Clinica`(`nro_hist`)
+     CONSTRAINT `histc` FOREIGN KEY (`nro_hist`) REFERENCES `Historia_Clinica`(`nro_hist`) ON DELETE CASCADE
 )ENGINE InnoDB;
 
 
@@ -135,8 +135,6 @@ CREATE TABLE `Recetado`(
 	`nombre_med` VARCHAR(15) PRIMARY KEY NOT NULL,
     `nro_item` INTEGER NOT NULL AUTO_INCREMENT,
     `dosis`	VARCHAR(20),
-    CONSTRAINT `rec` FOREIGN KEY (`nro_item`) REFERENCES `Episodio_Salud`(`nro_item`),
-    CONSTRAINT `rec2` FOREIGN KEY (`nombre_med`) REFERENCES `Medicamento`(`nombre_med`)
+    CONSTRAINT `rec` FOREIGN KEY (`nro_item`) REFERENCES `Episodio_Salud`(`nro_item`) ON DELETE CASCADE,
+    CONSTRAINT `rec2` FOREIGN KEY (`nombre_med`) REFERENCES `Medicamento`(`nombre_med`) ON DELETE CASCADE
 )ENGINE InnoDB;
-
-
